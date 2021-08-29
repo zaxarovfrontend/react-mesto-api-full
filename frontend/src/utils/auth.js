341f76a6abe29fc7dcb84ae2e410c5c37fd04708
Vladimir <zaxarov.frontend@gmail.com>
2021-08-29 18:37:34 +0400

    Test

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://backend.nomoredomains.rocks/'
+export const BASE_URL = 'https://backend.nomoredomains.rocks'
 
 const checkRes = (res) => {
     return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
