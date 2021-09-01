4c51e8ae79cb5ebaa957924b62250c835c1c064b
Vladimir <zaxarov.frontend@gmail.com>
2021-09-01 21:30:35 +0400

    Crash test

--- backend/app.js
+++ backend/app.js
@@ -44,12 +44,6 @@ require('dotenv').config();
 app.use(helmet());
 app.use('/', express.json());
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -66,6 +60,12 @@ app.post('/signin', celebrate({
     password: Joi.string().required(),
   }),
 }), login);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(auth);
 
